c52af83d26597306fdf413d23408c018cdde7331
Steph Locke <steph.locke@censornet.com>
2016-03-17 13:33:37 +0000

    Passthrough

--- R/HIBP_headers.R
+++ R/HIBP_headers.R
@@ -2,13 +2,14 @@
 #' [Optional] Change the agent to make the request bespoke to you.
 #'
 #' @param agent The user agent the API call should run under
+#' @param ...
 #'
 #' @return headers
 #' @export
 #'
 #' @examples
 #' HIBP_headers()
-HIBP_headers<-function(agent="HIBPwned R pkg"){
+HIBP_headers<-function(agent="HIBPwned R pkg",...){
   httr::add_headers(
      `api-version` = 2
     ,`User-Agent` = agent
