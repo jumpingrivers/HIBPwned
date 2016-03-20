d033308a1284187e31f1a13fbd10eb425d102282
Steph Locke <steph.locke@censornet.com>
2016-03-20 09:39:15 +0000

    Built breached_sites functions

---
+++ R/breached_site.R
@@ -0,0 +1,27 @@
+#' Get a specific breached site, based in breach name (not domain)
+#'
+#' @param name Search for a specific named breach
+#' @param ... Optional passthrough to HIBP_headers()
+#'
+#' @return Data.frame containing breach details
+#' @export
+#'
+#' @examples
+#' breached_site("Adobe")
+breached_site<-function(name,...){
+  if(is.null(name) | length(name)!=1 | !inherits(name,"character")) stop("Problematic breach name")
+  URLS<-"https://haveibeenpwned.com/api/breach/"
+  encoded<-urltools::url_encode(name)
+  URLS<-paste0(URLS ,encoded)
+  res<- GETcontent(URLS, HIBP_headers(...))
+  if(length(res)==1) res<-list(Title=NA,Name=name,Domain=NA,
+                                            BreachDate=NA,AddedDate=NA,PwnCount=NA,
+                                            Description=NA,DataClasses=NA,IsVerified=NA,
+                                            IsSensitive=NA,LogoType=NA )
+
+  res$DataClasses<-paste(res$DataClasses, sep=", ", collapse=", ")
+  res<-data.frame(res)
+
+  return(res)
+
+}
---
+++ tests/testthat/test-breached_site.R
@@ -0,0 +1,32 @@
+context("breached_site")
+
+test_that("breached_site works",{
+
+  # Simplest usage scenario
+  res<-breached_site("Adobe")
+
+  expect_is(res, "data.frame")
+  expect_gt(ncol(res),1)
+  expect_equal(nrow(res),1)
+
+  # With name (no breach)
+  res<-breached_site("abc")
+
+  expect_is(res, "data.frame")
+  expect_gt(ncol(res),1)
+  expect_equal(nrow(res),1)
+
+  # With header passthrough
+  expect_error(breached_site("Adobe",agent="blah"),NA)
+
+})
+
+
+test_that("handles incorrect values",{
+  errors<-"Problematic breach name"
+
+  expect_error(breached_site(TRUE),errors)
+  expect_error(breached_site(as.character(c())),errors)
+  expect_error(breached_site(rep(errors,2)),errors)
+
+})
