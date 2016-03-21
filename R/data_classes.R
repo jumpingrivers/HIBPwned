b57da922db4ecf3b9346fcadef329d7bc49bd141
Steph Locke <steph.locke@censornet.com>
2016-03-21 13:55:15 +0000

    Added data_classes

---
+++ R/data_classes.R
@@ -0,0 +1,15 @@
+#' Get list of classes of data that have been
+#' exposed in breaches
+#'
+#' @param ... Optional passthrough to HIBP_headers()
+#'
+#' @return Data.frame containing data class details
+#' @export
+#'
+#' @examples
+#' data_classes()
+data_classes<-function(...){
+  URLS<-"https://haveibeenpwned.com/api/dataclasses"
+  res<- GETcontent(URLS, HIBP_headers(...))
+  return(res)
+}
---
+++ tests/testthat/test-data_classes.R
@@ -0,0 +1,10 @@
+context("data_classes")
+
+test_that("data_classes works",{
+  res<-data_classes()
+  expect_is(res, "character")
+  expect_gte(length(res),48)
+
+  # With header passthrough
+  expect_error(data_classes(agent="blah"),NA)
+})
