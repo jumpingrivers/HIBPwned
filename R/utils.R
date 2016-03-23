017b12ff8028bca07b002489a40de30f94fb5a94
Steph Locke <steph.locke@censornet.com>
2016-03-23 10:44:52 +0000

    Added encoding Will prevent encoding messages

--- R/utils.R
+++ R/utils.R
@@ -4,7 +4,8 @@
 #'
 #' @return data.frame
 GETcontent<-function(...){
-  content<-httr::content(httr::GET(...),as="text")
+  content<-httr::content(httr::GET(...) ,as="text"
+                         ,encoding="utf-8")
   if(content=="") resp<-data.frame(name=NA)
   if(content!="") resp<-jsonlite::fromJSON(content)
   return(resp)
