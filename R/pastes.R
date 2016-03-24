f1763f5f85c4920208f544e9c712234176032200
Steph Locke <steph.locke@censornet.com>
2016-03-24 09:11:16 +0000

    Added pastes

---
+++ R/pastes.R
@@ -0,0 +1,27 @@
+#' Search for cases where an email address has been included in
+#' a paste
+#'
+#' @param emails A character vector of email addresses
+#' @param ... Optional passthrough to HIBP_headers()
+#'
+#' @return List of data.frames containing results
+#' @export
+#'
+#' @examples
+#' pastes(c("steff.sullivan@gmail.com","stephanie.g.locke@gmail.com"))
+pastes<-function(
+   emails
+  ,...){
+
+  if(length(emails)==0|!inherits(emails,"character")) stop("Problematic emails")
+
+  encoded<-urltools::url_encode(emails)
+  URLS<-paste0("https://haveibeenpwned.com/api/pasteaccount/"
+               ,encoded)
+
+  res<-lapply(URLS, GETcontent, HIBP_headers(...))
+  names(res)<-emails
+
+  return(res)
+
+}
---
+++ tests/testthat/test-pastes.R
@@ -0,0 +1,48 @@
+context("pastes")
+
+test_that("pastes works for a single account",{
+
+acct<-"steff.sullivan@gmail.com"
+
+# Simplest usage scenario
+res<-pastes(acct)
+
+expect_is(res, "list")
+expect_named(res, acct)
+expect_equal(length(res),1)
+expect_is(res[[acct]],"data.frame")
+expect_gte(ncol(res[[acct]]),1)
+expect_gte(nrow(res[[acct]]),1)
+
+# With header passthrough
+expect_error(pastes(acct,agent="blah"),NA)
+})
+
+test_that("pastes works for multiple accounts",{
+
+  acct<-c("steff.sullivan@gmail.com","stephanie.g.locke@gmail.com")
+
+  # Simplest usage scenario
+  res<-pastes(acct)
+
+  expect_is(res, "list")
+  expect_named(res, acct)
+  expect_equal(length(res),length(acct))
+  for(i in 1:length(acct)){
+    a<-acct[i]
+    expect_is(res[[a]],"data.frame")
+    expect_gte(ncol(res[[a]]),1)
+    expect_gte(nrow(res[[a]]),1)
+  }
+
+  # With header passthrough
+  expect_error(pastes(acct,agent="blah"),NA)
+})
+
+test_that("handles incorrect values",{
+  errors<-"Problematic emails"
+
+  expect_error(pastes(TRUE),errors)
+  expect_error(pastes(as.character(c())),errors)
+
+})
