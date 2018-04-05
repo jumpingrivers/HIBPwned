3b8bd9c100e7ac864e3e93407d6172625c6e4f08
Maëlle Salmon <maelle.salmon@yahoo.se>
2018-04-05 15:45:33 +0200

    close #16 by replacing httr with crul

--- R/utils.R
+++ R/utils.R
@@ -1,11 +1,13 @@
 #' Used to extract content from a GET request and collapse to a data.frame
 #' @description It calls a helper function that retries up to 6 times
 #'
+#' @param URLS urls
+#' @param headers headers as built by HIBP_headers()
 #' @param ... Pass through arguments
 #'
 #' @return data.frame
-GETcontent <- function(...) {# nolint
-  output <- get_and_retry(...)
+GETcontent <- function(URLS, headers,...) {# nolint
+  output <- get_and_retry(URLS, headers, ...)
   try_number <- 1
   while (is.null(output) && try_number < 6) {
     try_number <- try_number + 1
@@ -16,18 +18,20 @@ GETcontent <- function(...) {# nolint
   return(output)
 }
 
+crul_get <- function(url, headers){
+  crul::HttpClient$new(url = url,
+                       headers = headers)$get()
+}
+
 get <- ratelimitr::limit_rate(
-  httr::GET,
+  crul_get,
   ratelimitr::rate(n = 1, period = 1.6)
 )
 
 get_and_retry <- function(...) {# nolint
   resp <- get(...)
-  code <- httr::status_code(resp)
-  content <- httr::content(
-    resp, as = "text"
-    , encoding = "utf-8"
-  )
+  code <- resp$status_code
+  content <- resp$parse(encoding = "UTF-8")
   if (code > 200){
     # this will happen when no result
     if (code == 404){
