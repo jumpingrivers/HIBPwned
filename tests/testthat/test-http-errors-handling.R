dc2d3faf35a4cfe60bd9b090a0290811b7996859
Maëlle Salmon <maelle.salmon@yahoo.se>
2018-04-12 11:51:56 +0200

    long lines

--- tests/testthat/test-http-errors-handling.R
+++ tests/testthat/test-http-errors-handling.R
@@ -3,7 +3,7 @@ context("http error handling")
 test_that("no 404 http errors are handled as expected", {
   skip_on_cran()
   webmockr::enable()
-  stub <- webmockr::stub_request("get", "https://haveibeenpwned.com/api/breaches")
+  stub <- webmockr::stub_request("get", "https://haveibeenpwned.com/api/breaches") # nolint
   webmockr::to_return(stub, status = 429)
   expect_message(HIBPwned::breached_sites(), "Try")
   expect_silent(HIBPwned::breached_sites(verbose = FALSE))
@@ -16,7 +16,7 @@ test_that("no 404 http errors are handled as expected", {
 test_that("404 http errors are handled as expected", {
   skip_on_cran()
   webmockr::enable()
-  stub <- webmockr::stub_request("get", "https://haveibeenpwned.com/api/breaches")
+  stub <- webmockr::stub_request("get", "https://haveibeenpwned.com/api/breaches") # nolint
   webmockr::to_return(stub, status = 404)
   output <- HIBPwned::breached_sites()
   expect_is(output, "data.frame")
