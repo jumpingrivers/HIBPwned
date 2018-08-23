8bbcd256b38cdad2ca7f50a3a7b0a0615b74e4da
Maëlle Salmon <maelle.salmon@yahoo.se>
2018-08-23 16:16:26 +0200

    Delete test-lintr.R

--- tests/testthat/test-lintr.R
+++
@@ -1,8 +0,0 @@
-context("lintr")
-if (requireNamespace("lintr", quietly = TRUE)) {
-  context("lints")
-  test_that("Package Code Style", {
-    skip_on_cran()
-    lintr::expect_lint_free()
-  })
-}
